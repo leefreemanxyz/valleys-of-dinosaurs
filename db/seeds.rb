182857727004b3690600c216ce414ce0be8197a4
leefreemanxyz <lee.freeman1@gmail.com>
2017-03-09 11:47:53 +0100

    seeds updated to add random ages to each dinosaur

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
-dino1 = Dinosaur.create!( { name: "Iguanodon", age: 3, image_url: "https://goo.gl/wB6q9q" } )
-dino2 = Dinosaur.create!( { name: "Mosasaurus", age: 10, image_url: "https://goo.gl/aoZsYt" } )
-dino3 = Dinosaur.create!( { name: "Hungarosaurus", age: 15, image_url: "https://goo.gl/EqRlgP" } )
+def random_age
+  rand(100)
+end
+
+
+dino1 = Dinosaur.create!( { name: "Iguanodon", age: random_age, image_url: "https://goo.gl/wB6q9q" } )
+dino2 = Dinosaur.create!( { name: "Mosasaurus", age: random_age, image_url: "https://goo.gl/aoZsYt" } )
+dino3 = Dinosaur.create!( { name: "Hungarosaurus", age: random_age, image_url: "https://goo.gl/EqRlgP" } )
